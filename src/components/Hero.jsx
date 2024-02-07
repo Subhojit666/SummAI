baf7740d721dd412938049a06225f72d4a882490
Subhojit666 <subhojitghosh666@gmail.com>
2024-02-07 14:46:56 +0530

    small fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,12 +20,11 @@ const Hero = () => {
       </nav>
 
       <h1 className='head_text'>
-        Summarize Articles with <br className='max-md:hidden' />
-        <span className='orange_gradient '>OpenAI GPT-4</span>
+        Summarize <br className='max-md:hidden' />
+        <span className='orange_gradient '>Articles</span>
       </h1>
       <h2 className='desc'>
-        Simplify your reading with Summize, an open-source article summarizer
-        that transforms lengthy articles into clear and concise summaries
+      By just pasting the link
       </h2>
     </header>
   );
